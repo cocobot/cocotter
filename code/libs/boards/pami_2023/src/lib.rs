98ee7248921ee64a7a94361e19f7eb70a6a57c31
Damien Espitallier <de.electronique@gmail.com>
2024-12-03 22:35:06 +0100

    Add encoder devices

--- code/libs/boards/pami_2023/src/lib.rs
+++ code/libs/boards/pami_2023/src/lib.rs
@@ -3,14 +3,17 @@
 use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
 use embedded_hal::digital::PinState;
-use esp_hal::{gpio::{Io, Level, Output}, i2c::master::{Config, I2c}, peripherals::I2C0, timer::timg::TimerGroup, Async};
+use esp_hal::{gpio::{Input, Level, Output, Pull}, i2c::master::{Config, I2c}, pcnt::{self, Pcnt}, peripherals::PCNT, timer::timg::TimerGroup, Async};
 use esp_hal::prelude::*;
 use pwm_pca9685::{Address as Pca9685Address, Channel, Pca9685};
 use static_cell::StaticCell;
 use tca6408a::Tca6408a;
+use core::cmp::min;
 
 //declare hardcoded peripheral types
-type I2C0PamiDevice = I2cDevice<'static, CriticalSectionRawMutex, I2c<'static, Async>>;
+pub type I2C0PamiDevice = I2cDevice<'static, CriticalSectionRawMutex, I2c<'static, Async>>;
+pub type LeftWheelEncoder = pcnt::unit::Counter<'static, 0>;
+pub type RightWheelEncoder = pcnt::unit::Counter<'static, 1>;
 
 //external
 pub const PWM_EXTENDED_CHANEL_SERVO     : [Channel; 4]  = [Channel::C0, Channel::C1, Channel::C2, Channel::C3];
@@ -21,6 +24,7 @@ pub const PWM_EXTENDED_VBAT_RGB         : [Channel; 3]  = [Channel::C10, Channel
 pub const PWM_EXTENDED_LINE_LED         : Channel       = Channel::C11;
 pub const PWM_EXTENDED_LED_RGB          : [Channel; 3]  = [Channel::C12, Channel::C13, Channel::C14];
 
+
 pub struct Pami2023 {
     pub led_esp: Option<Output<'static>>,
     pub led_com: Option<Output<'static>>,
@@ -28,41 +32,101 @@ pub struct Pami2023 {
     pub line_sensor: Option<Tca6408a<I2C0PamiDevice>>,
     pub buttons: Option<Tca6408a<I2C0PamiDevice>>,
 
+    pub left_wheel_counter: Option<LeftWheelEncoder>,
+    pub right_wheel_counter: Option<RightWheelEncoder>,
+
     pub generic_i2c: I2C0PamiDevice,
 }
 
-pub fn board_init() -> Pami2023 {
-    let peripherals = esp_hal::init(esp_hal::Config::default());
+impl Pami2023 {
+    fn init_counters(pcnt: PCNT, left_pin_a: Input, left_pin_b: Input, right_pin_a: Input, right_pin_b: Input) -> (pcnt::unit::Counter<'static, 0>, pcnt::unit::Counter<'static, 1>) {
+        let pcnt = Pcnt::new(pcnt);
 
-    //init embassy
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-    esp_hal_embassy::init(timg0.timer0);
-
-    //init peripherals
+        let u0 = pcnt.unit0;
+        u0.set_filter(Some(min(10u16 * 80, 1023u16))).unwrap();
+        u0.clear();
     
-    let i2c0 = I2c::new(peripherals.I2C0, {
-        let mut config = Config::default();
-        config.frequency = 100.kHz();
-        config
-    }).with_scl(peripherals.GPIO17).with_sda(peripherals.GPIO18).into_async();
-    static I2C0_BUS: StaticCell<Mutex<CriticalSectionRawMutex, I2c<'static, Async>>> = StaticCell::new();
-    let i2c0 = I2C0_BUS.init(Mutex::new(i2c0));
+        let ch0 = &u0.channel0;
+        ch0.set_ctrl_signal(left_pin_a.peripheral_input());
+        ch0.set_edge_signal(left_pin_b.peripheral_input());
+        ch0.set_ctrl_mode(pcnt::channel::CtrlMode::Reverse, pcnt::channel::CtrlMode::Keep);
+        ch0.set_input_mode(pcnt::channel::EdgeMode::Increment, pcnt::channel::EdgeMode::Decrement);
+    
+        let ch1 = &u0.channel1;
+        ch1.set_ctrl_signal(left_pin_b.peripheral_input());
+        ch1.set_edge_signal(left_pin_a.peripheral_input());
+        ch1.set_ctrl_mode(pcnt::channel::CtrlMode::Reverse, pcnt::channel::CtrlMode::Keep);
+        ch1.set_input_mode(pcnt::channel::EdgeMode::Decrement, pcnt::channel::EdgeMode::Increment);
 
-    //init i2c peripherals
-    let pwm_extended = match Pca9685::new(I2cDevice::new(i2c0), Pca9685Address::from(0b110_1001)) {
-        Ok(pwm) => Some(pwm),
-        Err(_) => None,
-    };
-    let line_sensor = Some(Tca6408a::new(I2cDevice::new(i2c0), tca6408a::Address::from_pin_state(PinState::Low)));
-    let buttons = Some(Tca6408a::new(I2cDevice::new(i2c0), tca6408a::Address::from_pin_state(PinState::High)));
+        let u1 = pcnt.unit1;
+        u1.set_filter(Some(min(10u16 * 80, 1023u16))).unwrap();
+        u1.clear();
+    
+        let ch0 = &u1.channel0;
+        ch0.set_ctrl_signal(right_pin_a.peripheral_input());
+        ch0.set_edge_signal(right_pin_b.peripheral_input());
+        ch0.set_ctrl_mode(pcnt::channel::CtrlMode::Reverse, pcnt::channel::CtrlMode::Keep);
+        ch0.set_input_mode(pcnt::channel::EdgeMode::Increment, pcnt::channel::EdgeMode::Decrement);
+    
+        let ch1 = &u1.channel1;
+        ch1.set_ctrl_signal(right_pin_b.peripheral_input());
+        ch1.set_edge_signal(right_pin_a.peripheral_input());
+        ch1.set_ctrl_mode(pcnt::channel::CtrlMode::Reverse, pcnt::channel::CtrlMode::Keep);
+        ch1.set_input_mode(pcnt::channel::EdgeMode::Decrement, pcnt::channel::EdgeMode::Increment);
 
-    Pami2023 { 
-        led_esp: Some(Output::new(peripherals.GPIO4, Level::High)),
-        led_com: Some(Output::new(peripherals.GPIO5, Level::High)),
-        pwm_extended,
-        line_sensor,
-        buttons,
+        u0.listen();
+        u0.resume();
+        u1.listen();
+        u1.resume();
+    
+        let counter_left = u0.counter.clone();
+        let counter_right = u1.counter.clone();
+        
+        (counter_left, counter_right)
+    }
 
-        generic_i2c: I2cDevice::new(i2c0),
+    pub fn new() -> Self {
+        let peripherals = esp_hal::init(esp_hal::Config::default());
+
+        //init embassy
+        let timg0 = TimerGroup::new(peripherals.TIMG0);
+        esp_hal_embassy::init(timg0.timer0);
+
+        //init peripherals
+        let i2c0 = I2c::new(peripherals.I2C0, {
+            let mut config = Config::default();
+            config.frequency = 100.kHz();
+            config
+        }).with_scl(peripherals.GPIO17).with_sda(peripherals.GPIO18).into_async();
+        static I2C0_BUS: StaticCell<Mutex<CriticalSectionRawMutex, I2c<'static, Async>>> = StaticCell::new();
+        let i2c0 = I2C0_BUS.init(Mutex::new(i2c0));
+
+        //init i2c peripherals
+        let pwm_extended = match Pca9685::new(I2cDevice::new(i2c0), Pca9685Address::from(0b110_1001)) {
+            Ok(pwm) => Some(pwm),
+            Err(_) => None,
+        };
+        let line_sensor = Some(Tca6408a::new(I2cDevice::new(i2c0), tca6408a::Address::from_pin_state(PinState::Low)));
+        let buttons = Some(Tca6408a::new(I2cDevice::new(i2c0), tca6408a::Address::from_pin_state(PinState::High)));
+
+        //init motor peripherals
+        let left_wheel_pin_a = Input::new(peripherals.GPIO39, Pull::None);
+        let left_wheel_pin_b = Input::new(peripherals.GPIO40, Pull::None);
+        let right_wheel_pin_a = Input::new(peripherals.GPIO41, Pull::None);
+        let right_wheel_pin_b = Input::new(peripherals.GPIO42, Pull::None);
+        let (left_wheel_counter, right_wheel_counter) = Self::init_counters(peripherals.PCNT, left_wheel_pin_a, left_wheel_pin_b, right_wheel_pin_a, right_wheel_pin_b);
+
+        Self { 
+            led_esp: Some(Output::new(peripherals.GPIO4, Level::High)),
+            led_com: Some(Output::new(peripherals.GPIO5, Level::High)),
+            pwm_extended,
+            line_sensor,
+            buttons,
+
+            left_wheel_counter: Some(left_wheel_counter),
+            right_wheel_counter: Some(right_wheel_counter),
+
+            generic_i2c: I2cDevice::new(i2c0),
+        }
     }
 }
--- code/projects/pami/src/main.rs
+++ code/projects/pami/src/main.rs
@@ -1,13 +1,12 @@
 #![no_std]
 #![no_main]
 
-use board_pami_2023::{board_init, PWM_EXTENDED_LED_RGB};
+use board_pami_2023::{Pami2023, PWM_EXTENDED_LED_RGB, PWM_EXTENDED_VBAT_RGB};
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::gpio::Output;
 use esp_hal_embassy::main;
-use embedded_hal_async::i2c::I2c;
 
 extern crate alloc;
 use core::mem::MaybeUninit;
@@ -28,34 +27,67 @@ fn init_heap() {
 #[embassy_executor::task]
 async fn heartbeat(mut led: Output<'static>) {
     loop {
-        log::info!("heatbeat low !");
+        log::info!("heatbeat low !\n");
         led.set_low();
         Timer::after(Duration::from_millis(500)).await;
 
-        log::info!("heatbeat high !");
+        log::info!("heatbeat high !\n");
         led.set_high();
         Timer::after(Duration::from_millis(500)).await;
     }
 }
 
+#[embassy_executor::task]
+async fn asserv(left_wheel_counter: board_pami_2023::LeftWheelEncoder, right_wheel_counter: board_pami_2023::RightWheelEncoder) {
+    loop {
+        log::info!("left encoder value: {} / right encoder value: {}", left_wheel_counter.get(), right_wheel_counter.get());
+        Timer::after(Duration::from_millis(100)).await;
+    }
+}
+
+/* 
+#[embassy_executor::task]
+async fn adc_mon(mut adc: PamiAdc) {
+    loop {
+        log::info!("heatbeat low !\n");
+        led.set_low();
+        Timer::after(Duration::from_millis(500)).await;
+
+        log::info!("heatbeat high !\n");
+        led.set_high();
+        Timer::after(Duration::from_millis(500)).await;
+    }
+}
+*/
+
 #[main]
-async fn main(spawner: Spawner) {        
-    let mut board = board_init();
+async fn main(spawner: Spawner) {       
+    /*let peripherals = esp_hal::init(esp_hal::Config::default());
+
+    //init embassy
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_hal_embassy::init(timg0.timer0);
+
+    init_heap();
+
+    esp_println::logger::init_logger_from_env();
+
+    let pin39 = Input::new(peripherals.GPIO39, Pull::None);
+    let pin40 = Input::new(peripherals.GPIO40, Pull::None);
+
+    loop {
+        log::info!("pin39: {} / pin40 {}", pin39.is_high(), pin40.is_high());
+        Timer::after(Duration::from_millis(25)).await;
+    }*/
+
+    let mut board = Pami2023::new();
     init_heap();
 
     esp_println::logger::init_logger_from_env();
 
     spawner.spawn(heartbeat(board.led_esp.take().unwrap())).unwrap();
+    spawner.spawn(asserv(board.left_wheel_counter.take().unwrap(), board.right_wheel_counter.take().unwrap())).unwrap();
 
-    log::info!("Check for I2C devices");
-    for addr in 0..127 {
-        let mut buf = [0u8; 1];
-        let r = board.generic_i2c.read(addr, &mut buf).await;
-        if r.is_ok() {
-            log::info!("Found device at address 0x{:02x}", addr);
-        }
-    }
-    
     //configure pwm
     let mut pwm_extended = board.pwm_extended.take();
     if let Some(pwm_extended) = pwm_extended.as_mut() {
@@ -70,9 +102,28 @@ async fn main(spawner: Spawner) {
         pwm_extended.set_channel_on(PWM_EXTENDED_LED_RGB[2], 0).await.ok();
     }
     
+    //let mut adc = board.adc.take().unwrap();
+
     //run color blind test
     loop {
-        
+        //const VBATT_RL_KOHMS : f32= 91.0;
+        //const VBATT_RH_KOHMS : f32= 91.0;
+        //let vbatt_raw : u16 = adc.adc.read_oneshot(&mut adc.vbatt).unwrap();
+        //let vbatt_mv : f32 = (vbatt_raw as f32)/((1<<12 -1) as f32) * 1750.0 * (1.0 + VBATT_RH_KOHMS/VBATT_RL_KOHMS);
+        let vbatt_mv = 5.0;
+        log::info!("BATT : battery voltage : {}mV", vbatt_mv);
+
+        // lifepo4 discharge example:  https://cleversolarpower.com/lifepo4-voltage-chart/
+        const VBATT_LVL_WARN_MV : f32 = 3150.0; // corresponds to around 15% SOC battery SOC
+        const VBATT_LVL_ERR_MV  : f32 = 3000.0; // corresponds to around 5% battery SOC
+
+        let vbat_color :[u16; 3];
+        match vbatt_mv {
+            mv if mv <= VBATT_LVL_ERR_MV  => vbat_color = [ 2048, 0,    0   ], // error : stop required
+            mv if mv <= VBATT_LVL_WARN_MV => vbat_color = [ 2048, 2048, 0   ], // low battery
+            _                             => vbat_color = [ 0,    0,    4095], // ok
+        }
+
         
         if let Some(btns) = board.buttons.as_mut() {
             let state = btns.get_input().await.ok();
@@ -83,6 +134,11 @@ async fn main(spawner: Spawner) {
         }
 
         if let Some(pwm_extended) = pwm_extended.as_mut() {
+
+            pwm_extended.set_channel_on(PWM_EXTENDED_VBAT_RGB[0], vbat_color[0]).await.unwrap();
+            pwm_extended.set_channel_on(PWM_EXTENDED_VBAT_RGB[1], vbat_color[1]).await.unwrap();
+            pwm_extended.set_channel_on(PWM_EXTENDED_VBAT_RGB[2], vbat_color[2]).await.unwrap();
+
             pwm_extended.set_channel_off(PWM_EXTENDED_LED_RGB[0], 2047).await.ok();
             pwm_extended.set_channel_off(PWM_EXTENDED_LED_RGB[1], 4095).await.ok();
             pwm_extended.set_channel_off(PWM_EXTENDED_LED_RGB[2], 4095).await.ok();
