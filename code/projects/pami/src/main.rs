984029da0643214299398d6575fe0e35f7a5c179
lamygalle <lamygalle@gmail.com>
2024-12-13 21:56:25 +0100

    pami : demo code to display accelerometer data

--- code/projects/pami/src/main.rs
+++ code/projects/pami/src/main.rs
@@ -19,6 +19,7 @@ use ui::{UIEvent, UI};
 extern crate alloc;
 use core::mem::MaybeUninit;
 
+use libm::{cosf, sinf};
 
 fn init_heap() {
     const HEAP_SIZE: usize = 32 * 1024;
@@ -88,30 +89,90 @@ async fn main(spawner: Spawner) {
     spawner.spawn(analog_reading(ui.clone(), board.adc.take().unwrap())).unwrap();
     log::info!("analog reading created!");
 
-    spawner.spawn(asserv(board.left_wheel_counter.take().unwrap(), board.right_wheel_counter.take().unwrap())).unwrap();
+    //spawner.spawn(asserv(board.left_wheel_counter.take().unwrap(), board.right_wheel_counter.take().unwrap())).unwrap();
 
     let mut accel = board.accelerometer.take().unwrap();
     
     //spawner.spawn(test_vaccum()).unwrap();
-    let mut vaccumm_lvl : u16;
+    //let mut vaccumm_lvl : u16;
+
+    let mut left_motor_pwm = board.left_motor_pwm.take().unwrap();
+
+    PWM::send_event(PWMEvent::LineLedLvl(1.0));
+    accel.init();
+    let mut speed : u16 = 0;
 
     //run color blind test
     loop {        
         
-        //accel.update_measures();
+        accel.update_measures();
         //let angle = accel.get_angular_rate();
-        //log::info!("accelerometer angle {} {} {}", angle.x, angle.y, angle.z);
+        let angle = accel.get_angular_rate();
+        let acc = accel.get_acceleration();
+        let temp = accel.get_temperature_degc();
+        log::info!("accelerometer accel {:4} {:4} {:4} \t{:4} {:4} {:4} \t{:2.3}", angle.x, angle.y, angle.z, acc.x, acc.y, acc.z, temp);
         if let Some(btns) = board.buttons.as_mut() {
             let state = btns.get_input().await.ok();
+            match state{ 
+            
+            Some(i) => {
+                let mut st : [u8; 8] = [0;8];
+
+                for n in 0..7{
+                    if i & ((1<<n) as u8)==0{
+                        st[n] = 0;
+                    }else {
+                        st[n] = 1;
+                    }
+                    
+                }
+                log::info!("Buttons state: {:?}", state);
+            },
+            _=> log::info!("autre"),
+        }
            /* match state{
                 Some(i:u8) => 
 
             };*/
-            log::info!("Buttons state: {:?}", state);
+            
+        
         }
         else {
             log::info!("No buttons found");
         }
+
+        if let Some(lines) = board.line_sensor.as_mut() {
+            let state = lines.get_input().await.ok();
+            match state{ 
+            
+                Some(i) => {
+                    let mut st : [u8; 8] = [0;8];
+    
+                    for n in 0..=7{
+                        if i & ((1<<n) as u8)==0{
+                            st[n] = 1;
+                        }else {
+                            st[n] = 0;
+                        }
+                        
+                    }
+                    log::info!("line state: {:?}, r3 {}, r2 {}, r1 {}, r0 {}, l0 {}, l1 {}, l2 {}, l3 {}", state, st[0], st[1], st[2], st[3], st[4], st[5], st[6], st[7]);
+                },
+                _=> log::info!("autre"),
+            }
+            //log::info!("lines state: {:?}", state);
+        }
+        else {
+            log::info!("No buttons found");
+        }
+
+        left_motor_pwm.0.set_timestamp(speed);
+        left_motor_pwm.1.set_timestamp(0);
+        speed += 5;
+        if speed >= 100{
+            speed = 0;
+        }
+        log::info!("speed : {speed}");
 
         PWM::send_event(PWMEvent::LedBottom([0.5, 0.0, 0.0]));
         Timer::after(Duration::from_millis(250)).await;
