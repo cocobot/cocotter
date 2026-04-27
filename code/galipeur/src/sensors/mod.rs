43d580a8ad381f1c2d6e822a3938d8873e511559
Damien Espitallier <de.electronique@gmail.com>
2026-04-28 00:34:47 +0200

    add debug

--- code/galipeur/src/sensors/mod.rs
+++ code/galipeur/src/sensors/mod.rs
@@ -270,6 +270,9 @@ impl<B: SabotterBoard + 'static> Sensors<B> {
             return None;
         }
 
+         log::info!("GroundLidarModule for {:?}: distance_0={} sq_0={} distance_1={} sq_1={}",
+            side, module.distance_0, module.sq_0, module.distance_1, module.sq_1);
+
         let conf = self.ground_lidar_conf.get()?.modules[idx];
 
         let pose0 = conf[0];
