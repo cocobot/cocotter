a28a4450c551dd06791fe43fe1efed32229b9f46
Augustin <augustin.boisvert@gmail.com>
2026-05-03 17:53:12 +0200

    pami: remove NimBLE info level debug

--- code/boards/pami/src/esp.rs
+++ code/boards/pami/src/esp.rs
@@ -64,7 +64,10 @@ impl PamiBoard for EspPamiBoard {
 
     fn init() -> Self {
         esp_idf_svc::sys::link_patches();
-        esp_idf_svc::log::EspLogger::initialize_default();
+
+        let logger = esp_idf_svc::log::init_from_esp_idf();
+        logger.filter().set_target_level("NimBLE", log::LevelFilter::Warn).unwrap();
+
         let _nvs = EspDefaultNvsPartition::take().unwrap();
 
         let peripherals = Peripherals::take().unwrap();
